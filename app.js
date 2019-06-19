10b7fb3073414edb123c3565b094be37c4094dbf
dlim303 <deli4631@colorado.edu>
2019-06-19 14:16:52 -0600

    basic game var/dl

--- app.js
+++ app.js
@@ -8,3 +8,19 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6 + 1);
+console.log(dice)
+
+document.querySelector('#current-' + activePlayer).textContent = dice; //  setters
+// document.querySelector('#current-' + activePlayer).textContent = '<em>' + dice + '<\em>';
+var x = document.querySelector('#score-0').textContent; //  getters
+console.log(x)
+
+document.querySelector('.dice').style.display = 'none'; // removes the dice of the screen
